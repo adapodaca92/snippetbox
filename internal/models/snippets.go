3912a62d37dc8fa4433df327c1259e2285cb560d
Anthony Apodaca <adapodaca92@gmail.com>
2024-01-21 18:33:56 -0800

    Added middleware.

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -6,9 +6,6 @@ import (
 	"time"
 )
 
-// Define a Snippet type to hold the data for an individual snippet. Notice how
-// the fields of the struct correspond to the fields in our MySQL snippets
-// table?
 type Snippet struct {
 	ID      int
 	Title   string
